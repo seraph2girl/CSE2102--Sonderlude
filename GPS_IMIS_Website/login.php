e747aff29ca336874285fe8ee08f594b68eeb4c7
Jornelyearwood <43959969+Jornelyearwood@users.noreply.github.com>
2018-11-11 23:12:08 -0400

    Add files via upload

--- GPS_IMIS_Website/login.php
+++ GPS_IMIS_Website/login.php
@@ -4,7 +4,7 @@ session_start();
 
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: welcome.php");
+    header("location: ../index.php");
     exit;
 }
 
@@ -79,7 +79,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["position"] = $position;
 
                             // Redirect user to welcome page
-                            header("location: welcome.php");
+                            header("location: ../index.php");
                         } else{
                             // Display an error message if password is not valid
                             $password_err = "The password you entered was not valid.";
@@ -106,37 +106,36 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    
     <title>Login</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
+    <link rel="stylesheet" type="text/css" href="style.css">
+   
     </style>
 </head>
 <body>
     <div class="wrapper">
-        <h2>Login to IMIS</h2>
-        <p>Please fill in your credentials to login.</p>
+        <h2><center>Login to IMIS</center></h2>
+        <p><center>Please fill in your credentials to login.</center></p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group <?php echo (!empty($name_err)) ? 'has-error' : ''; ?>">
                 <label>Name</label>
-                <input type="text" name="name" class="form-control" value="<?php echo $name; ?>">
+               <center> <input type="text" name="name" class="form-control" value="<?php echo $name; ?>"></center>
                 <span class="help-block"><?php echo $name_err; ?></span>
               </div>
             <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
                 <label>Username</label>
-                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+               <center> <input type="text" name="username" class="form-control" value="<?php echo $username; ?>"></center>
                 <span class="help-block"><?php echo $username_err; ?></span>
             </div>
             <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
                 <label>Password</label>
-                <input type="password" name="password" class="form-control">
+               <center> <input type="password" name="password" class="form-control"></center>
                 <span class="help-block"><?php echo $password_err; ?></span>
             </div>
             <div class="form-group <?php echo (!empty($position_err)) ? 'has-error' : ''; ?>">
-                <label>Position</label>
-                <input type="text" name="position" class="form-control" value="<?php echo $position; ?>">
+              <label>Position</label>
+             <center>   <input type="text" name="position" class="form-control" value="<?php echo $position; ?>"></center>
                 <span class="help-block"><?php echo $position_err; ?></span>
               </div>
             <div class="form-group">
@@ -147,3 +146,4 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     </div>
 </body>
 </html>
+
